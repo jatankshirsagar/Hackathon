b68c6b1910a037268f99569303af0e9f1b72480f
JatanPC <jatan.kshirsagar@gmail.com>
2017-09-17 01:12:41 +0530

    updated gradle and other files

--- app/src/main/java/com/inadev/ekyc/LoginActivity.java
+++ app/src/main/java/com/inadev/ekyc/LoginActivity.java
@@ -1,19 +1,32 @@
 package com.inadev.ekyc;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by OPTLPTP163 on 9/16/2017.
- */
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        firebaseCode();
+    }
+
+    private void firebaseCode() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = getString(R.string.google_app_id);
+            String channelName = getString(R.string.gcm_defaultSenderId);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
+
+        Utils.showLog("subscribed");
     }
 }
