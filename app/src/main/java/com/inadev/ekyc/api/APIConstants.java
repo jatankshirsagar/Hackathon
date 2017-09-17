9fd1827d063bcd4a9b8f9b82b232289df5d3013d
JatanPC <jatan.kshirsagar@gmail.com>
2017-09-17 08:53:23 +0530

    updated gradle and other files

--- app/src/main/java/com/inadev/ekyc/api/APIConstants.java
+++ app/src/main/java/com/inadev/ekyc/api/APIConstants.java
@@ -1,11 +1,9 @@
 package com.inadev.ekyc.api;
 
-/**
- * Created by OPTLPTP163 on 9/16/2017.
- */
 
 class APIConstants {
-    public static final String BASE_URL = "http://10.2.120.147/dcbbank/";
+    //public static final String BASE_URL = "http://10.2.120.147/dcbbank/";
+    public static final String BASE_URL = "http://like-labs.com/dcbbank/dcbbank/";
     public static final String PATH_LOGIN = "loginapi.php";
     public static final String PATH_TRANSACTION = "response.php";
     public static final String HEADER_CONTENT_TYPE = "Content-Type:application/json";
