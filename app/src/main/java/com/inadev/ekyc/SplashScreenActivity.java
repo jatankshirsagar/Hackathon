7a144e98a50e6da4fb5435217710912f685b8ab5
Pranav Tank <pranav@optimizeitsystems.com>
2017-09-17 07:55:41 +0530

    Retrofit and login

--- app/src/main/java/com/inadev/ekyc/SplashScreenActivity.java
+++ app/src/main/java/com/inadev/ekyc/SplashScreenActivity.java
@@ -1,27 +1,52 @@
 package com.inadev.ekyc;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    @BindView(R.id.progressbar)
+    ProgressBar progressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        ButterKnife.bind(this);
+        startProgress();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        (new Handler()).postDelayed(new Runnable() {
+    private void startProgress() {
+        ObjectAnimator animation = ObjectAnimator.ofInt(progressbar, "progress", 0, 100);
+        animation.setDuration(5000);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.addListener(new Animator.AnimatorListener() {
             @Override
-            public void run() {
+            public void onAnimationStart(Animator animator) { }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                //do something when the countdown is complete
                 startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
                 finish();
             }
-        }, 3000);
+
+            @Override
+            public void onAnimationCancel(Animator animator) { }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) { }
+        });
+        animation.start();
     }
+
+
 }
